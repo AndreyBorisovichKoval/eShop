7ecc14ebde5973cffdd906151e11e31f40f9c163
AndreyBorisovichKoval <andreyako57@gmail.com>
2024-09-09 15:55:25 +0500

    [refactor] Modify files...

--- pkg/controllers/helpers.go
+++ pkg/controllers/helpers.go
@@ -14,38 +14,19 @@ func handleError(c *gin.Context, err error) {
 	switch {
 	case errors.Is(err, errs.ErrUsernameUniquenessFailed),
 		errors.Is(err, errs.ErrIncorrectUsernameOrPassword):
+		// Ошибка уникальности или неверного пароля
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
 	case errors.Is(err, errs.ErrRecordNotFound):
+		// Ошибка "Запись не найдена"
 		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+
 	case errors.Is(err, errs.ErrPermissionDenied):
+		// Ошибка "Доступ запрещен"
 		c.JSON(http.StatusForbidden, gin.H{"error": err.Error()})
+
 	default:
+		// Здесь просто возвращаем внутреннюю ошибку клиенту
 		c.JSON(http.StatusInternalServerError, gin.H{"error": errs.ErrSomethingWentWrong.Error()})
 	}
 }
-
-// // C:\GoProject\src\eShop\pkg\controllers\helpers.go
-
-// package controllers
-
-// import (
-// 	"eShop/errs"
-// 	"errors"
-// 	"net/http"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// func handleError(c *gin.Context, err error) {
-// 	if errors.Is(err, errs.ErrUsernameUniquenessFailed) ||
-// 		errors.Is(err, errs.ErrIncorrectUsernameOrPassword) {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 	} else if errors.Is(err, errs.ErrRecordNotFound) {
-// 		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-// 	} else if errors.Is(err, errs.ErrPermissionDenied) {
-// 		c.JSON(http.StatusForbidden, gin.H{"error": err.Error()})
-// 	} else {
-
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": errs.ErrSomethingWentWrong.Error()})
-// 	}
-// }
