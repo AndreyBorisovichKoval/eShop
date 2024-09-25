06d37a9f3dbb672b8f6f9d3ffeff562389a26955
AndreyBorisovichKoval <andreyako57@gmail.com>
2024-09-25 16:12:34 +0500

    [refactor] Modify files... 0023

--- pkg/repository/log_user_request.go
+++ pkg/repository/log_user_request.go
@@ -7,19 +7,6 @@ import (
 	"eShop/models"
 )
 
-// // GetUserByID - функция, которая получает пользователя по его ID
-// func GetUserByID(userID uint) (*models.User, error) {
-// 	var user models.User
-
-// 	// Запрос к базе данных для получения пользователя по ID
-// 	err := db.GetDBConn().Where("id = ?", userID).First(&user).Error
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &user, nil
-// }
-
 // LogRequestHistory - функция для записи данных в таблицу RequestHistory
 func LogRequestHistory(requestHistory *models.RequestHistory) error {
 	// Запись данных в таблицу RequestHistory
