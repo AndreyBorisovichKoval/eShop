481b74d5ec3af80c925a6e219ca033d9d8503c8a
AndreyBorisovichKoval <andreyako57@gmail.com>
2024-10-07 14:07:01 +0500

    [refactor] Modify files... New_0001

--- pkg/service/test_orders.go
+++ pkg/service/test_orders.go
@@ -1,3 +1,5 @@
+// // C:\GoProject\src\eShop\pkg\service\test_orders.go
+
 package service
 
 import (
@@ -55,13 +57,19 @@ func GenerateRandomOrders(count int) error {
 			// Выбираем случайный товар
 			product := products[rand.Intn(len(products))]
 
+			// Генерируем случайное количество товара
+			quantity := float64(rand.Intn(6) + 1) // Случайное количество от 1 до 6
+
+			// Правильное вычисление суммы для элемента заказа
+			total := product.RetailPrice * quantity
+
 			// Создаем элемент заказа
 			orderItem := models.OrderItem{
 				OrderID:   order.ID,
 				ProductID: product.ID,
-				Quantity:  float64(rand.Intn(6) + 1), // Случайное количество от 1 до 6
-				Price:     product.RetailPrice,
-				Total:     product.RetailPrice * float64(rand.Intn(6)+1),
+				Quantity:  quantity,            // Количество товара
+				Price:     product.RetailPrice, // Цена товара
+				Total:     total,               // Общая сумма для элемента заказа (Цена * Количество)
 			}
 
 			// Добавляем элемент заказа в базу данных
@@ -71,6 +79,7 @@ func GenerateRandomOrders(count int) error {
 				return err
 			}
 
+			// Увеличиваем общую сумму заказа
 			totalAmount += orderItem.Total
 		}
 
@@ -84,3 +93,92 @@ func GenerateRandomOrders(count int) error {
 
 	return nil
 }
+
+// // C:\GoProject\src\eShop\pkg\service\test_orders.go
+
+// package service
+
+// import (
+// 	"eShop/errs"
+// 	"eShop/logger"
+// 	"eShop/models"
+// 	"eShop/pkg/repository"
+// 	"math/rand"
+// 	"time"
+// )
+
+// // GenerateRandomOrders генерирует случайные заказы для тестирования
+// func GenerateRandomOrders(count int) error {
+// 	// Получаем всех доступных пользователей (продавцов)
+// 	users, err := repository.GetAllUsers()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Получаем все доступные товары
+// 	products, err := repository.GetAllProducts()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Если нет продавцов или товаров, невозможно создать заказы
+// 	if len(users) == 0 || len(products) == 0 {
+// 		return errs.ErrSomethingWentWrong
+// 	}
+
+// 	// Генерация случайных заказов
+// 	for i := 0; i < count; i++ {
+// 		// Выбираем случайного пользователя (продавца)
+// 		user := users[rand.Intn(len(users))]
+
+// 		// Создаем новый заказ
+// 		order := models.Order{
+// 			UserID:    user.ID,
+// 			IsPaid:    true, // По умолчанию заказы считаем оплаченными
+// 			CreatedAt: time.Now(),
+// 		}
+
+// 		// Добавляем заказ в базу данных
+// 		err := repository.CreateOrder(&order)
+// 		if err != nil {
+// 			logger.Error.Printf("[service.GenerateRandomOrders] error creating order: %v", err)
+// 			return err
+// 		}
+
+// 		// Генерируем случайное количество товаров в заказе (от 1 до 6)
+// 		productCount := rand.Intn(6) + 1
+// 		var totalAmount float64
+
+// 		for j := 0; j < productCount; j++ {
+// 			// Выбираем случайный товар
+// 			product := products[rand.Intn(len(products))]
+
+// 			// Создаем элемент заказа
+// 			orderItem := models.OrderItem{
+// 				OrderID:   order.ID,
+// 				ProductID: product.ID,
+// 				Quantity:  float64(rand.Intn(6) + 1), // Случайное количество от 1 до 6
+// 				Price:     product.RetailPrice,
+// 				Total:     product.RetailPrice * float64(rand.Intn(6)+1),
+// 			}
+
+// 			// Добавляем элемент заказа в базу данных
+// 			err := repository.CreateOrderItem(&orderItem)
+// 			if err != nil {
+// 				logger.Error.Printf("[service.GenerateRandomOrders] error creating order item: %v", err)
+// 				return err
+// 			}
+
+// 			totalAmount += orderItem.Total
+// 		}
+
+// 		// Обновляем общую сумму заказа
+// 		order.TotalAmount = totalAmount
+// 		err = repository.UpdateOrder(order)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	return nil
+// }
